402ddf6d099e960ef1a23085d14453dacbe2408b
jggomeztocinouca <jesus.gomeztocino@alum.uca.es>
2023-11-23 19:32:49 +0100

    Algunos miniarreglos

--- Crawler/WikipediaCrawler.java
+++ Crawler/WikipediaCrawler.java
@@ -31,7 +31,7 @@ public class WikipediaCrawler {
      * Inicia el proceso de crawling.
      * Crea un hilo para cada descarga y espera a que todos los hilos terminen.
      *
-     * @param semilla URL de inicio del crawling.
+     * @param semilla           URL de inicio del crawling.
      * @param profundidadMaxima Profundidad máxima de crawling.
      */
     public void iniciaCrawling(URL semilla, int profundidadMaxima) {
@@ -41,34 +41,48 @@ public class WikipediaCrawler {
 
         planificador.encolarURL(semilla, 0);
 
-        while (!planificador.vacia() || descargasActivas.get() > 0) {
+        while (!planificador.vacia() || descargasActivas.get() > 0) { // El pool de hilos estará activo mientras haya URLs por procesar o descargas activas.
             moduloPlanificacion.tuplaProfundidadURL tupla = planificador.siguienteURL();
-            if (tupla != null && tupla.profundidad <= profundidadMaxima && !almacenamiento.almacenada(tupla.url)) {
-                descargasActivas.incrementAndGet();
-                poolHilos.submit(() -> {
+            if (tupla != null) { // UPDATE: No es necesario comprobar si está almacenada
+                descargasActivas.incrementAndGet(); // Si se decide procesar la URL, se incrementa el contador de descargas activas.
+                poolHilos.submit(() -> { // Función lambda que crea un hilo para procesar la URL.
                     new moduloDescarga(tupla.url, profundidadMaxima, planificador, almacenamiento).run();
-                    descargasActivas.decrementAndGet();
+                    descargasActivas.decrementAndGet(); // Una vez procesada la URL, se decrementa el contador de descargas activas.
                 });
             }
         }
 
         poolHilos.shutdown();
-        while(!poolHilos.isTerminated());
+        while (!poolHilos.isTerminated()) ; // Se espera a que se cierre correctamente el pool de hilos.
 
         Instant instanteFinal = Instant.now();
         long tiempoTranscurrido = Duration.between(instanteInicial, instanteFinal).toSeconds();
         System.out.println("Crawling finalizado. Tiempo total: " + tiempoTranscurrido + " segundos.");
     }
 
+    /**
+     * Verifica si una URL es válida.
+     * Una URL válida es aquella que pertenece a Wikipedia en español.
+     *
+     * @param urlStr URL a verificar.
+     * @return true si la URL es válida, false en caso contrario.
+     */
     private static boolean URLvalida(String urlStr) {
         return urlStr.matches("https://es\\.wikipedia\\.org/wiki/.*");
     }
 
+    /**
+     * Método main de WikipediaCrawler.
+     * Solicita al usuario la URL de inicio y la profundidad máxima de crawling.
+     * Inicia el proceso de crawling.
+     *
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la URL de inicio:");
-        String seedStr = scanner.nextLine();
-        if (!URLvalida(seedStr)) {
+        String semillaStr = scanner.nextLine();
+        if (!URLvalida(semillaStr)) {
             System.out.println("URL inválida o no pertenece a Wikipedia en español.");
             return;
         }
@@ -77,7 +91,7 @@ public class WikipediaCrawler {
         scanner.close();
 
         try {
-            URL semilla = new URL(seedStr);
+            URL semilla = new URL(semillaStr);
             WikipediaCrawler crawler = new WikipediaCrawler();
             crawler.iniciaCrawling(semilla, profundidadMaxima);
         } catch (Exception e) {
--- Crawler/moduloDescarga.java
+++ Crawler/moduloDescarga.java
@@ -13,75 +13,21 @@ import java.util.regex.Pattern;
 public class moduloDescarga implements Runnable {
     // Atributos
     private URL url;
-    private int profundidadMaxima;
     private moduloPlanificacion planificador;
     private moduloAlmacenamiento almacenamiento;
 
     /**
      * Constructor del moduloDescarga.
      *
-     * @param url URL a descargar.
+     * @param url               URL a descargar.
      * @param profundidadMaxima Profundidad máxima de crawling.
-     * @param planificador Instancia del planificador para añadir nuevas URLs.
-     * @param almacenamiento Instancia del almacenamiento para guardar los contenidos descargados.
+     * @param planificador      Instancia del planificador para añadir nuevas URLs.
+     * @param almacenamiento    Instancia del almacenamiento para guardar los contenidos descargados.
      */
     public moduloDescarga(URL url, int profundidadMaxima, moduloPlanificacion planificador, moduloAlmacenamiento almacenamiento) {
         this.url = url;
-        this.profundidadMaxima = profundidadMaxima;
         this.planificador = planificador;
         this.almacenamiento = almacenamiento;
-    }
-
-    /**
-     * Método run que se ejecutará cuando el hilo comience.
-     * Realiza la descarga del contenido de la URL y procesa dicho contenido.
-     */
-    @Override
-    public void run() {
-        System.out.println("Procesando URL: " + url);
-        try {
-            HttpURLConnection conexionHTTP = (HttpURLConnection) url.openConnection();
-            conexionHTTP.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conexionHTTP.getInputStream()));
-            String inputLine;
-            StringBuilder contenido = new StringBuilder();
-
-            while ((inputLine = reader.readLine()) != null) {
-                contenido.append(inputLine);
-                contenido.append(System.lineSeparator());
-            }
-
-            reader.close();
-            procesarContenido(contenido.toString());
-            almacenamiento.almacenarPagina(url, contenido.toString());
-
-        } catch (Exception e) {
-            System.out.println("Error al conectar con la URL: " + url);
-        }
-    }
-
-    /**
-     * Procesa el contenido descargado de la URL, extrayendo nuevas URLs y añadiéndolas al planificador.
-     *
-     * @param contenido Contenido de la página descargada.
-     */
-    private void procesarContenido(String contenido) {
-        Pattern pattern = Pattern.compile("href=\"(/wiki/[^:()]*?)\"");
-        Matcher matcher = pattern.matcher(contenido);
-
-        while (matcher.find()) {
-            try {
-                URL URLencontrada = new URL("https://es.wikipedia.org" + matcher.group(1));
-                System.out.println("URL encontrada: " + URLencontrada);
-                int profundidadActual = planificador.obtenerProfundidad(url);
-                if (mismoDominio(URLencontrada)) {
-                    planificador.encolarURL(URLencontrada, profundidadActual + 1);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     /**
@@ -92,5 +38,60 @@ public class moduloDescarga implements Runnable {
      */
     private boolean mismoDominio(URL url) {
         return url.getHost().endsWith("wikipedia.org");
+    }
+
+    /**
+     * Procesa el contenido descargado de la URL, extrayendo nuevas URLs y añadiéndolas al planificador.
+     *
+     * @param contenido Contenido de la página descargada.
+     */
+    private void procesarContenido(String contenido) {
+        Pattern pattern = Pattern.compile("href=\"(/wiki/[^:()]*?)\""); // Ruta relativa de la URL de la página de Wikipedia
+        Matcher matcher = pattern.matcher(contenido); // Busca todas las coincidencias de la expresión regular en el contenido de la página
+
+        while (matcher.find()) { // Mientras haya coincidencias, se añaden las URLs al planificador
+            try {
+                URL URLencontrada = new URL("https://es.wikipedia.org" + matcher.group(1)); // Se construye la URL completa, capturando el grupo 1
+                // de la expresión regular
+                //System.out.println("URL encontrada: " + URLencontrada);
+                int profundidadActual = planificador.obtenerProfundidad(url); // Se obtiene la profundidad de la URL actual
+                if (mismoDominio(URLencontrada)) { // Si la URL es interna (del mismo dominio), se añade al planificador
+                    planificador.encolarURL(URLencontrada, profundidadActual + 1); // +1 porque la profundidad de la URL encontrada es 1 mayor que
+                    // la actual
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Método run que se ejecutará cuando el hilo comience.
+     * Realiza la descarga del contenido de la URL y procesa dicho contenido.
+     */
+    @Override
+    public void run() {
+        System.out.println("Procesando URL: " + url);
+        try {
+            HttpURLConnection conexionHTTP = (HttpURLConnection) url.openConnection(); // Establece conexión con la URL
+            conexionHTTP.setRequestMethod("GET"); // Establece el método de la petición a GET (Obtener)
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conexionHTTP.getInputStream()));
+            // Lee el contenido de la página línea por línea y lo almacena en un StringBuilder
+            String inputLine;
+            StringBuilder contenido = new StringBuilder(); // Más eficiente para almacenar cadenas de caracteres que un String
+
+            while ((inputLine = reader.readLine()) != null) { // Lee el contenido de la página línea por línea hasta que no haya más contenido
+                contenido.append(inputLine); // Almacena la línea leída en el StringBuilder
+                contenido.append(System.lineSeparator()); // Añade un salto de línea al final de la línea leída
+            }
+
+            reader.close();
+            procesarContenido(contenido.toString()); // Procesa el contenido de la página en busca de nuevas URLs
+            almacenamiento.guardarArchivo(url, contenido.toString()); // Guarda el contenido de la página en un archivo
+
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la URL: " + url);
+        }
     }
 }
